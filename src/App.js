d637e82c0e2f5147691e018bcb90d9edd6cf380d
Jae Kim <jaekim0519@gmail.com>
2023-09-23 14:43:29 -0400

    a2 CSS and Bootstrap second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Web Dev Assignment 1
-        </h1>
+        <h1>Web Development CS4550</h1>
         <ul>
-          <li><a href="https://nimble-bavarois-bc9ae2.netlify.app/labs/a1/index">Lab 1</a></li>
-          <li><a href="https://nimble-bavarois-bc9ae2.netlify.app/kanbas/menu">Assignment 1 - Menu</a></li>
-          <li><a href="https://nimble-bavarois-bc9ae2.netlify.app/labs/a2/index">Lab 2</a></li>
+          <li>
+            <a href="https://nimble-bavarois-bc9ae2.netlify.app/labs/a1/index">
+              Lab 1
+            </a>
+          </li>
+          <li>
+            <a href="https://nimble-bavarois-bc9ae2.netlify.app/labs/a2/index">
+              Lab 2
+            </a>
+          </li>
+          <li>
+            <a href="https://nimble-bavarois-bc9ae2.netlify.app/kanbas/index">
+              Assignment 2 - Menu
+            </a>
+          </li>
         </ul>
       </header>
     </div>
