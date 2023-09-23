316ccc15733932b26a2bae69b0367a30f48e026a
Jae Kim <jaekim0519@gmail.com>
2023-09-23 14:38:20 -0400

    a2 CSS and Bootstrap

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <ul>
           <li><a href="https://nimble-bavarois-bc9ae2.netlify.app/labs/a1/index">Lab 1</a></li>
           <li><a href="https://nimble-bavarois-bc9ae2.netlify.app/kanbas/menu">Assignment 1 - Menu</a></li>
+          <li><a href="https://nimble-bavarois-bc9ae2.netlify.app/labs/a2/index">Lab 2</a></li>
         </ul>
       </header>
     </div>
